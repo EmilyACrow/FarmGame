d35566dcccbe9aa835a6e79b34f0bdaf5fc834c6
fulge <fulge@192.168.1.7>
2020-04-30 05:04:19 +1200

    Another practice piece

---
+++ FarmSimulatorTest/src/farmLand/FarmType.java
@@ -0,0 +1,13 @@
+package farmLand;
+
+/**
+ * FarmType is an enumerator that holds the constant variables that refer to the types of farms available.
+ *Last modified: 30/04/2020
+ *
+ *Created 29/04/2020
+ * @author Kenn Leen Duenas Fulgencio
+
+ */
+public enum FarmType {
+	COMMERCIAL, PRODUCE, RANCH, MIXED;
+}
