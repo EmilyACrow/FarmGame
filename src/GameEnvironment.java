b0346d3fef31e2e2729877a4ed8c40ed107600c2
fulge <fulge@192.168.1.9>
2020-05-16 22:24:53 +1200

    Scanner added to ask player for farm creation

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -1,18 +1,65 @@
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 public class GameEnvironment {
 	private ArrayList<Merchandise> m_selectedMerchandise;
 	private Farm m_farm;
 	private GeneralStore m_store;
 	
+	//scanner as an attribute
+	private Scanner askPlayer;
+	
+	
+	/**
+	 * the constructor method calls methods that use the scanner to prompt player to input data 
+	 * required to create the Farm class. 
+	 */
 	public GameEnvironment()
 	{
+		askPlayer = new Scanner(System.in);
+		
+		System.out.println("Enter the farmer's name:");
+		String farmerName = askName();
+		
+		
+		System.out.println("Enter the farmer's age:");
+		int farmerAge = askAge();
+			
+		
+		//create a Farmer instance
+		Farmer farmer = new Farmer(farmerName, farmerAge);
+		
+		System.out.println("Enter name of Farm:");
+		String farmName = askName();
+		
+		
+		
+		System.out.println("Enter number of a type of farm.");
+		System.out.println("Pick from: "+
+				"\n1.Ranch" +
+				"\n2.Commercial" +
+				"\n3Produce" +
+				"\n4Mixed");
+		FarmType farmType = askFarmType();
+		
+		
+		//Creates a new game
+		createNewGame(farmName, farmType, farmer);
+		
+				
+		
 	}
 	
 	public void Run()
@@ -208,5 +255,257 @@ public class GameEnvironment {
 	private void endGame()
 	{
 		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * Methods for prompting the player with the Scanner begin below.
+	 * 
+	 */
+	
+	
+	
+	
+	
+	/**
+	 * This method asks the player to input a name.
+	 * @return the farmer's name.
+	 */
+	public String askName() {
+		
+		//this avoids the 'variable may not be initialized' error
+		String name = "kiwifruit";
+		
+		Boolean isValid = false;
+		
+		//This will continue asking for a name if not valid
+		
+		while(!(isValid)) {
+			name = askPlayer.next();
+			name += askPlayer.nextLine();
+			
+			
+			isValid = isValidInput(name);
+		
+		}
+		
+		return name;
+	
+		}
+	
+	
+	/**
+	 * Ask user to input age for farmer.
+	 * @return farmer's age
+	 */
+	public int askAge() {
+		//this avoids the 'variable may not be initialized' error
+		int farmerAge = numberOnly();
+		
+		return farmerAge;
+	}
+	
+	
+	/**
+	 * ask player to enter a number corresponding to a type of farm.
+	 * 
+	 * @return a number that represents a farmType's value
+	 */
+	public FarmType askFarmType() {
+		
+		//avoids the variable is NULL error.
+		FarmType farmType = FarmType.RANCH;
+		int playerSelection = 0;
+		
+		//keeps asking player until a valid number is chosen.
+		while((!(playerSelection == 1)) && (!(playerSelection == 2)) && (!(playerSelection == 3) && (!(playerSelection == 4)) )) {
+			
+			System.out.println("enter a number above:");
+			playerSelection = numberOnly();
+			
+			
+			if((!(playerSelection == 1)) && (!(playerSelection == 2)) && (!(playerSelection == 3) && (!(playerSelection == 4)) )) {
+				
+				System.out.println(playerSelection + " is not a valid number.");
+			}
+			
+			
+		}
+		
+		
+		
+		//Depending on the number selected, it will assign the Farm Type. 
+		if (playerSelection == 1) {
+			farmType = FarmType.RANCH;
+		}
+		
+		else if (playerSelection == 2) {
+			farmType = FarmType.COMMERCIAL;
+		}
+		
+		else if (playerSelection == 3) {
+			farmType = FarmType.PRODUCE;
+		}
+		
+		else if (playerSelection == 4) {
+			farmType = FarmType.MIXED;
+		}
+
+		
+		return farmType;
+	}
+	
+	
+
+	
+	/*
+	 * Methods that check if the player's input is valid begin below here
+	 * 
+	 */
+	
+	
+	
+	/**
+	 * Only returns a player's input if it is an Integer. 
+	 * @return an integer
+	 */
+	public int numberOnly() {
+		
+		//this avoids the 'variable may not be initialized' error
+		int validNumber = 100;
+		
+		Boolean isValid = false;
+		
+		//loop back to the beginning of the try-catch block. 
+		while(!(isValid)) {
+			
+			try {
+				
+				//scanner only accepts an integer.
+				validNumber = askPlayer.nextInt();
+				
+			}
+			
+			//tells player if they enter something that isn't a number.
+			catch(InputMismatchException e) {
+				System.out.println( "invalid input. Please only enter a number: ");
+				
+				//assigns any skipped lines to nothing. so it should leave the scanner free of any characters.
+				askPlayer.nextLine();
+				continue;
+			
+			}
+
+			//assigns any skipped lines to nothing. so it should leave the scanner free of any characters.
+			askPlayer.nextLine();
+			
+			isValid = true;
+			
+			
+		}
+		
+		return validNumber;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Checks if the string-value the player entered is valid.
+	 * 
+	 * @param playerInput Value player has typed in.
+	 * @return True: player input fits length and type requirements.
+	 */
+	public Boolean isValidInput(String playerInput) {
+		
+		
+		//First checks if there are only letters in the input
+		
+		for (int index = 0; index < playerInput.length(); index ++ ) {
+			
+			char check = playerInput.charAt(index);
+			
+			
+			if(Character.isAlphabetic(check)) {
+				continue; // next iteration
+				
+			}
+			
+			
+			else {
+				
+				System.out.println("please don't use any numbers, spaces or symbols. Input another name:");
+				return false;
+			}
+		}
+		
+		
+		
+		//Does it exceed 15 characters?
+		if (playerInput.length() > 15) {
+			
+			System.out.println(playerInput + " is too long. Max length is 15 char. Please input another name:");
+			return false;
+		}
+		
+		//Is it less than 3?
+		else if (playerInput.length() < 3) {
+			System.out.println(playerInput + " is too short! Min length is 3. Please input another name:");
+			return false;
+		}
+		
+		
+
+		else {
+			//It has passed through all the conditions. 
+			return true;
+		}
+		
+	}
+		
+	
+	
+	
+	
+	/*
+	* Getter and Setter methods begin below here
+	* 
+	*
+	*/
+	
+	
+	public Scanner getAskPlayer() {
+		return askPlayer;
+	}
+	
+	
+	
+	
+	
+	
+	/*
+	 * Run the game.
+	 */
+	public static void main(String[] args) {
+		
+		
+		//uncomment everything to run. 
+		//GameEnvironment newSession = new GameEnvironment();
+		
+		//System.out.println("Back from character creation");
+
+		
+		//closes the scanner once game finished.
+		//newSession.getAskPlayer().close();
+		
+
 	}
 }
